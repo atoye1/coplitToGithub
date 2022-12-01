6d77e6d69d48c90cc225c7a561b71ab4bebdaedf
SeolDonghun <ssalssi1@gmail.com>
2022-12-01 14:17:54 +0900

    unnecessary files removed

--- scripts/content.js
+++ scripts/content.js
@@ -3,28 +3,20 @@ const main = async () => {
   await delay(1000);
   console.log(window.location)
 
-  const header = document.getElementById('extensionIndicator');
-  if (!header) {
-    const h3 = document.createElement('h3');
-    h3.id = 'extentionIndicator'
-    h3.textContent = "크롬익스텐션 설정 되었습니다 content.js."
-    const rootElem = document.getElementById("root");
-    rootElem.insertBefore(h3, rootElem.firstChild);
-    document.title = 'CoplitToGithub Testing'
-  }
-
   const url = new URL(window.location.href);
   const problemHash = url.pathname.split('/')[2];
 
   const btnGroup = document.querySelector('div.item.end');
-  const submitBtn = document.querySelector("div.item.end")?.children[2];
 
   const githubBtn = document.createElement('button');
-  if (!githubBtn) return;
-  githubBtn.textContent = "Coplit to github"
-  githubBtn.classList = submitBtn.classList;
-  githubBtn.style = submitBtn.style;
-  githubBtn.style.backgroundColor = 'crimson';
+  githubBtn.textContent = "Commit To Github"
+  githubBtn.style.backgroundColor = "#ff9900"
+  githubBtn.style.color = "white"
+  githubBtn.style.height = "white"
+
+  githubBtn.style.padding = "10px"
+  githubBtn.style.borderRadius = "10px"
+  githubBtn.style.margin = "0px 10px"
   githubBtn.id = "githubBtn";
 
   btnGroup.appendChild(githubBtn);
@@ -32,33 +24,18 @@ const main = async () => {
   githubBtn.addEventListener('click', async () => {
     const scoreText = document.querySelector('div.codeproblem-console-content > div > div')?.textContent;
     if (scoreText === undefined || scoreText === '테스트 결과가 없습니다.') {
-      alert('채점후에 커밋하세요!')
+      alert('테스트 후에 커밋하세요!')
       return;
     }
     const fileName = document.querySelector('span.problem-title').textContent + '.js';
     const fileContent = localStorage.getItem(problemHash);
-    const inputCommitMessage = '커스텀 인풋메시지 입니다. 나중에 입력가능하게 바뀝니다.';
-    const commitMessage = scoreText + inputCommitMessage ? ', ' + inputCommitMessage.trim() : '';
+
+    //TODO 커밋 메시지 입력 가능하게 만든다.
+    const commitMessage = '';
     alert(commitMessage)
-    // 수동으로 accessToken을 등록하게 한다.
 
     const response = await chrome.runtime.sendMessage({ action: "Commit", fileName, fileContent, commitMessage });
     alert(response);
-  })
-
-  const storageTest = document.createElement('button');
-  storageTest.textContent = "StorageTest"
-  storageTest.classList = submitBtn.classList;
-  storageTest.style = submitBtn.style;
-  storageTest.style.backgroundColor = 'skyblue';
-  storageTest.id = "storageTest";
-  btnGroup.appendChild(storageTest);
-
-  storageTest.addEventListener('click', () => {
-    chrome.runtime.sendMessage({ action: "StorageTest" }, function (response) {
-      alert(response);
-      console.log(response);
-    });
   })
 }
 
@@ -69,3 +46,5 @@ const mainInterval = setInterval(() => {
     main();
   }
 }, 2000);
+
+// https://stackoverflow.com/questions/3522090/event-when-window-location-href-changes
